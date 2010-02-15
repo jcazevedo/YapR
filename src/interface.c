4730661af850bf1a0527bbdc7ec07bfb82ef64e0
Joao Azevedo <joao.c.azevedo@gmail.com>
2010-02-15 12:05:02 +0000

    double_val changed to float_val.

--- src/interface.c
+++ src/interface.c
@@ -4,7 +4,7 @@
 
 #define BUFSIZE 256
 
-static int double_val(void)
+static int float_val(void)
 {
     YAP_Term in = YAP_ARG1;
     YAP_Term out = YAP_ARG2;
@@ -37,7 +37,7 @@ static int init_r(void)
 
 void init_my_predicates()
 {
-    YAP_UserCPredicate("double_val", double_val, 2);
+    YAP_UserCPredicate("float_val", float_val, 2);
     YAP_UserCPredicate("int_val", int_val, 2);
     YAP_UserCPredicate("init_r", init_r, 0);
 }
